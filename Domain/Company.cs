511c976834097e00b64f9d46a25be886fd78f3fb
titas1111 <titasaleksandravicius@gmail.com>
2021-05-08 11:04:55 +0300

    Add project files.

---
+++ Domain/Company.cs
@@ -0,0 +1,16 @@
+﻿namespace efcore_domain_event.Domain
+{
+    public class Company
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public float Discount { get; private set; }
+
+
+        public Company(string name, float discount)
+        {
+            Name = name;
+            Discount = discount;
+        }
+    }
+}
